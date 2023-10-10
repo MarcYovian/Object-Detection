0ffc64fa6c668665e24790a2a73a8a081664425c
marcellinus Yovian Indrastata <marcellinusyovian@gmail.com>
2023-10-10 08:00:02 +0700

    count Car

--- Project 1 - Car Counter/Car-Counter.py
+++ Project 1 - Car Counter/Car-Counter.py
@@ -5,6 +5,9 @@ import cvzone
 import math
 from sort import *
 
+cap = cv2.VideoCapture(1)  # For Webcam
+cap.set(3, 1280)
+cap.set(4, 720)
 cap = cv2.VideoCapture("../Videos/cars.mp4")  # For Video
 
 model = YOLO("../Yolo-Weights/yolov8l.pt")
@@ -26,14 +29,8 @@ mask = cv2.imread("mask2.png")
 # Tracking
 tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
 
-limitsPlus = [450, 200, 673, 200]
-limitsMinus = [10, 600, 673, 600]
-totalCountIn = []
-totalCountOut = []
 total = 0
 
-# bbox = [300, 300, 673, 300]
-# objek_di_kotak = 0
 
 while True:
     success, img = cap.read()
@@ -44,7 +41,6 @@ while True:
     results = model(imgRegion, stream=True)
 
     detections = np.empty((0, 5))
-    print(detections)
 
     for r in results:
         boxes = r.boxes
@@ -52,12 +48,7 @@ while True:
             # Bounding Box
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
             w, h = x2 - x1, y2 - y1
-
-            # if bbox[0] <= x1 and bbox[1] <= y1 and bbox[2] >= x2 and bbox[3] >= y2:
-            #     # Objek terletak sepenuhnya dalam kotak, tambahkan ke hitungan
-            #     objek_di_kotak += 1
 
             # Confidence
             conf = math.ceil((box.conf[0] * 100)) / 100
@@ -65,20 +56,14 @@ while True:
             cls = int(box.cls[0])
             currentClass = classNames[cls]
 
-
             if currentClass == "car" or currentClass == "truck" or currentClass == "bus" \
-                    or currentClass == "motorbike" and conf > 0.3:
-                # cvzone.putTextRect(img, f'{currentClass} {conf}', (max(0, x1), max(35, y1)),
-                #                    scale=0.6, thickness=1, offset=3)
-                # cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=5)
+                    or currentClass == "motorbike":
                 currentArray = np.array([x1, y1, x2, y2, conf])
                 detections = np.vstack((detections, currentArray))
 
     resultsTracker = tracker.update(detections)
+    total = str(len(resultsTracker))
 
-    # cv2.line(img, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255,0,0), 3)
-    cv2.line(img, (limitsPlus[0], limitsPlus[1]), (limitsPlus[2], limitsPlus[3]), (0, 0, 255), 5)
-    cv2.line(img, (limitsMinus[0], limitsMinus[1]), (limitsMinus[2], limitsMinus[3]), (0, 255, 0), 5)
     for result in resultsTracker:
         x1, y1, x2, y2, id = result
         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
@@ -88,26 +73,7 @@ while True:
         cvzone.putTextRect(img, f' {int(id)}', (max(0, x1), max(35, y1)),
                            scale=1, thickness=2, offset=10)
 
-        cx, cy = x1 + w // 2, y1 + h // 2
-        cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
-
-        if limitsPlus[0] < cx < limitsPlus[2] and limitsPlus[1] - 15 < cy < limitsPlus[1] + 15:
-            if id not in totalCountIn:  # Periksa apakah ID belum ada dalam totalCount sebelum menambahkannya
-                totalCountIn.append(id)
-            cv2.line(img, (limitsPlus[0], limitsPlus[1]), (limitsPlus[2], limitsPlus[3]), (0, 255, 0), 5)
-
-        if limitsMinus[0] < cx < limitsMinus[2] and limitsMinus[1] - 15 < cy < limitsMinus[1] + 15:
-            if id not in totalCountOut:  # Periksa apakah ID ada dalam totalCount sebelum menghapusnya
-                totalCountOut.append(id)
-            cv2.line(img, (limitsMinus[0], limitsMinus[1]), (limitsMinus[2], limitsMinus[3]), (0, 0, 255), 5)
-
-    # cvzone.putTextRect(img, f' Count: {len(totalCount)}', (50, 50))
-    total = len(totalCountIn)-len(totalCountOut)
-    # print(total)
     cv2.putText(img,str(total),(255,100),cv2.FONT_HERSHEY_PLAIN,5,(50,50,255),8)
 
-
-    # cv2.putText(img, f'Objek di dalam kotak: {objek_di_kotak}', (50, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
     cv2.imshow("Image", img)
-    # cv2.imshow("ImageRegion", imgRegion)
     cv2.waitKey(1)
